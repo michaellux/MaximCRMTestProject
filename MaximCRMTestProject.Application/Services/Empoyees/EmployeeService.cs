b49b611d24f89c109e7993a50c3370b5c379ebe3
Sidachenko Michael <michaellux@yandex.ua>
2023-06-07 15:57:29 +0300

    add draft new contract methods and implementation

--- MaximCRMTestProject.Application/Services/Empoyees/EmployeeService.cs
+++ MaximCRMTestProject.Application/Services/Empoyees/EmployeeService.cs
@@ -2,8 +2,28 @@ namespace MaximCRMTestProject.Application.Services.Employees;
 
 public class EmployeeService : IEmployeeService
 {
-  public EmployeeResult Add(string fullName, string position)
-  {
-    return new EmployeeResult(Guid.NewGuid(), fullName, position);
-  }
+    public List<EmployeeResult> GetAll()
+    {
+        return new List<EmployeeResult>();
+    }
+
+    public EmployeeResult Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public EmployeeResult Add(string fullName, string position)
+    {
+        return new EmployeeResult(Guid.NewGuid(), fullName, position);
+    }
+
+    public EmployeeResult Update(Guid Id, string fullName, string position)
+    {
+        throw new NotImplementedException();
+    }
+
+    public EmployeeResult Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
--- MaximCRMTestProject.Application/Services/Empoyees/IEmployeeService.cs
+++ MaximCRMTestProject.Application/Services/Empoyees/IEmployeeService.cs
@@ -2,5 +2,9 @@ namespace MaximCRMTestProject.Application.Services.Employees;
 
 public interface IEmployeeService
 {
+    List<EmployeeResult> GetAll();
+    EmployeeResult Get(Guid id);
     EmployeeResult Add(string fullName, string position);
+    EmployeeResult Update(Guid Id, string fullName, string position);
+    EmployeeResult Delete(Guid id);
 }
